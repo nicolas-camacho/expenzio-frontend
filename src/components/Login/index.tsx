d3eec3bfcec818087fa2dffebdf027211cd8458c
Nicolas Camacho <nicolas.aguilar1999@gmail.com>
2023-09-26 21:14:10 -0500

    feat: create login component

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,12 +1,14 @@
 import { BaseButton } from "../Buttons"
 
+import "./login.scss"
+
 const Login = () => {
     return (
-        <>
+        <div className="login">
             <input placeholder="User" type="text"></input>
             <input type="password" name="password" placeholder="password" />
             <BaseButton onClick={() => null}>Log In</BaseButton>
-        </>
+        </div>
     )
 }
 
