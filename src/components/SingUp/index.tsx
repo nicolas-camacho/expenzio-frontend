3ebe0156f30f71032eae699f690d58c9eb1ea18f
Nicolas Camacho <48541640+nicolas-camacho@users.noreply.github.com>
2023-10-04 22:36:52 -0500

    feat: sing up completed also functionality implemented (#9)
    * feat: sing up completed also functionality implemented
    
    * Restyled by whitespace
    
    ---------
    
    Co-authored-by: Restyled.io <commits@restyled.io>

--- src/components/SingUp/index.tsx
+++ src/components/SingUp/index.tsx
@@ -1,16 +1,63 @@
-import { BaseButton } from "../Buttons"
+import { ChangeEvent, FormEvent, useContext, useState } from "react"
+import { BaseButton, LinkButton } from "../Buttons"
 import { BaseInput } from "../Inputs"
+import { PageContext } from "../../contexts/PageContext"
 
 import "./signup.scss"
 
+type userData = {
+    first_name: string;
+    last_name: string;
+    email: string;
+    password: string;
+    password_confirmation: string;
+}
+
 const SignUp = () => {
+    const {switchPages} = useContext(PageContext)
+    const [user, setUser] = useState<userData>({
+        first_name: "",
+        last_name: "",
+        email: "",
+        password: "",
+        password_confirmation: ""
+    })
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        e.stopPropagation()
+        const url = import.meta.env.VITE_API_URL
+        fetch(url + "user", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({user: user})
+        }).then(response => {
+            if (response.status === 200) {
+                switchPages()
+            } else {
+                console.error("cambia")
+            }
+        })
+    }
+
+    const handleChange = ({ target }: ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = target
+        setUser({...user, [name]: value})
+    }
+
+
     return (
-        <div className="authorization">
-            <BaseInput placeholder="User" type="text" />
-            <BaseInput type="password" name="password" placeholder="password" />
-            <BaseInput type="password" name="repeat password" placeholder="repeat password" />
-            <BaseButton onClick={() => null}>Sign Up</BaseButton>
-        </div>
+        <form className="authetication-container" onSubmit={handleSubmit}>
+            <BaseInput name="first_name" placeholder="First Name" type="text" onChange={handleChange} value={user.first_name} />
+            <BaseInput name="last_name" placeholder="Last Name" type="text" onChange={handleChange} value={user.last_name} />
+            <BaseInput name="email" placeholder="Email" type="text" onChange={handleChange} value={user.email} />
+            <BaseInput name="password" type="password" placeholder="password" onChange={handleChange} value={user.password} />
+            <BaseInput name="password_confirmation" type="password" placeholder="repeat password" onChange={handleChange} value={user.password_confirmation} />
+            <BaseButton type="submit">Sign Up</BaseButton>
+            <LinkButton onClick={switchPages}>I already have an account</LinkButton>
+        </form>
     )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { PageContextProvider } from './contexts/PageContext';
 import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <PageContextProvider>
+      <App />
+    </PageContextProvider>
   </React.StrictMode>,
 )
